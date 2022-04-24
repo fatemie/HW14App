da83c89120e66d06dd25006af0550ff8ff74c107
fateme <fs.fatemi@chmail.ir>
2022-04-24 04:46:26 +0430

    add splash screen

--- app/src/main/java/com/example/hw14app/MainActivity.kt
+++ app/src/main/java/com/example/hw14app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Dialog
 import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
@@ -23,6 +24,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        supportActionBar?.hide()
+        binding.iconSplash.alpha= 0f
+        binding.iconSplash.animate().setDuration(3000).alpha(1f).withEndAction {
+            binding.navHostFragment.visibility = View.VISIBLE
+            binding.iconSplash.visibility = View.GONE
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+        }
     }
 }
 
